d4bf7bc1b1461fec0b84ece311bb2949a04d448b
orlando123 <softorlando@gmail.com>
2021-07-15 15:23:22 -0400

    creando el modelo para compras

--- app/Models/Compra.php
+++ app/Models/Compra.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Compra extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'provider_id',
+        'usario_id',
+        'fecha_compra',
+        'impuesto',
+        'total',
+        'estado',
+        'picture',
+    ];
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function proveedor(){
+        return $this->belongsTo(Proveedor::class);
+    }
+    /*public function purchaseDetails(){
+        return $this->hasMany(PurchaseDetails::class);
+    }*/
 }
