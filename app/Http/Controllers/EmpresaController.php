69b6ededb9688bdb4ebf30f5df24e996dd8fa751
orlando123 <softorlando@gmail.com>
2021-08-03 17:31:06 -0400

    creacion de tabla modelo-controlador-seeder-EmpresaNegocio

---
+++ app/Http/Controllers/EmpresaController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Empresa;
+use Illuminate\Http\Request;
+
+class EmpresaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Empresa  $empresa
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Empresa $empresa)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Empresa  $empresa
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Empresa $empresa)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Empresa  $empresa
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Empresa $empresa)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Empresa  $empresa
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Empresa $empresa)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_08_03_212950_create_empresas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmpresasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empresas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empresas');
+    }
+}
